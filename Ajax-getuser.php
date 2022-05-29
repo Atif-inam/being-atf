a087718a0ed8cd0462045b27dc9cdc3beb587597
Atif-inam <106467620+Atif-inam@users.noreply.github.com>
2022-05-29 23:30:24 +0530

    Create Ajax-getuser.php

---
+++ Ajax-getuser.php
@@ -0,0 +1,25 @@
+<script>
+			var list = document.getElementById("users");
+			function getUsersList()
+			{
+				var ajaxobj = new XMLHttpRequest();
+				
+				ajaxobj.open("GET","https://jsonplaceholder.typicode.com/users", true);
+				ajaxobj.send();
+
+				ajaxobj.onreadystatechange = function()
+				{
+					if(ajaxobj.readyState == 4 && ajaxobj.status == 200)
+					{
+						var data = JSON.parse(ajaxobj.responseText);
+						var txt = "";
+						for(user of data)
+						{
+							txt += "<p>Name: "+user.username+" and "+user.email+"</p>";
+						}
+						
+						list.innerHTML = txt;
+					}
+				}
+			}
+		</script>
